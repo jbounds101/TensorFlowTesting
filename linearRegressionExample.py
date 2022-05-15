f2c55112660ab90731bd2778761ac0c14fbd9fd1
Jackson <jackson.bounds@gmail.com>
2022-05-15 18:03:53 -0400

    Linear regression testing pt2

--- linearRegressionExample.py
+++ linearRegressionExample.py
@@ -9,3 +9,51 @@ from six.moves import urllib
 import tensorflow.compat.v2.feature_column as fc
 
 import tensorflow as tf
+
+
+# Load dataset.
+dftrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.csv') # training data (used to
+# train)!! (loads in training data)
+dfeval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.csv') # testing data
+print(dftrain.head())  # prints the first 5 objects
+y_train = dftrain.pop('survived')  # removes survived column for data and returns that column to var
+y_eval = dfeval.pop('survived')
+print(dftrain.head())
+
+print(dftrain.describe())  # gives a bunch of different stats for the data set
+
+CATEGORICAL_COLUMNS = ['sex', 'n_siblings_spouses', 'parch', 'class', 'deck',
+                       'embark_town', 'alone']  # qualitative, needs to be converted to numerical values
+NUMERIC_COLUMNS = ['age', 'fare']  # quantitative
+
+feature_columns = []  # to be fed to estimator
+for feature_name in CATEGORICAL_COLUMNS:
+  vocabulary = dftrain[feature_name].unique()  # gets a list of all unique values from given feature column (for
+  # instance, sex would give [male, female]
+  feature_columns.append(tf.feature_column.categorical_column_with_vocabulary_list(feature_name, vocabulary))
+
+for feature_name in NUMERIC_COLUMNS:
+  feature_columns.append(tf.feature_column.numeric_column(feature_name, dtype=tf.float32))
+
+print(feature_columns)
+
+def make_input_fn(data_df, label_df, num_epochs=10, shuffle=True, batch_size=32):
+  def input_function():  # inner function, this will be returned
+    ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df))  # create tf.data.Dataset object with data and its label
+    if shuffle:
+      ds = ds.shuffle(1000)  # randomize order of data
+    ds = ds.batch(batch_size).repeat(num_epochs)  # split dataset into batches of 32 and repeat process for number of epochs
+    return ds  # return a batch of the dataset
+  return input_function  # return a function object for use
+
+train_input_fn = make_input_fn(dftrain, y_train)  # here we will call the input_function that was returned to us to get a dataset object we can feed to the model
+eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False)
+
+linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
+# We create a linear estimator by passing the feature columns we created earlier
+
+linear_est.train(train_input_fn)  # train
+result = linear_est.evaluate(eval_input_fn)  # get model metrics/stats by testing on testing data
+
+clear_output()  # clears console output
+print(result['accuracy'])  # the result variable is simply a dict of stats about our model
